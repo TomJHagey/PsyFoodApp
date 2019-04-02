ce1efe1d0e7257bdc7f7f7b7c2c607dd4f7c7516
Theo Eisenstein <theo.eisenstein@gmail.com>
2019-04-02 13:19:50 -0400

    Add config to ignore.

--- PsyFoodApp/src/main/java/com/example/PsyFoodApp/dao/PsyFoodDao.java
+++ PsyFoodApp/src/main/java/com/example/PsyFoodApp/dao/PsyFoodDao.java
@@ -1,6 +1,7 @@
 package com.example.PsyFoodApp.dao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,7 @@ import org.springframework.stereotype.Repository;
 @Transactional
 
 public class PsyFoodDao {
-
+	@PersistenceContext
 	private EntityManager em;
 	
 }
