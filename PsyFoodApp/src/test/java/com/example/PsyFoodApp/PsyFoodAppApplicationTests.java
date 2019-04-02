a8a34cda8b71ce6ed329aab31e3cce6f47af6ba4
TomJHagey <tomjhagey@gmail.com>
2019-04-02 12:21:01 -0400

    starting

---
+++ PsyFoodApp/src/test/java/com/example/PsyFoodApp/PsyFoodAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.PsyFoodApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PsyFoodAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
