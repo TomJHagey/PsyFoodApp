c86aefbbb9cb063d1d9803a9d389d9452a5e8024
TomJHagey <tomjhagey@gmail.com>
2019-04-02 12:46:21 -0400

    some

--- PsyFoodApp/src/main/java/com/example/PsyFoodApp/PsyFoodAppController.java
+++ PsyFoodApp/src/main/java/com/example/PsyFoodApp/PsyFoodAppController.java
@@ -2,13 +2,15 @@ package com.example.PsyFoodApp;
 
 import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class PsyFoodAppController {
 	
 	@RequestMapping("/")
-	public ModelAndView index(HttpSession session) {
+	public ModelAndView index() {
 		return new ModelAndView("index");
 	}
 
